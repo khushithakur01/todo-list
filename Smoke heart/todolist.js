f268d549c98e780c1deb42223c9511f4fcd4c2e3
khushithakur01 <123866444+khushithakur01@users.noreply.github.com>
2025-02-10 16:04:41 +0530

    Update todolist.js

--- Smoke heart/todolist.js
+++ Smoke heart/todolist.js
@@ -1,33 +1,46 @@
-document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task")
-    }
-    else{
+document.querySelector('#push').onclick = function () {
+    let taskInput = document.querySelector('#newtask input');
+    
+    if (taskInput.value.length == 0) {
+        alert("Please Enter a Task");
+    } else {
         document.querySelector('#tasks').innerHTML += `
             <div class="task">
-                <span id="taskname">
-                    ${document.querySelector('#newtask input').value}
-                </span>
-                <button class="delete">
-                    <i class="far fa-trash-alt"></i>
-                </button>
+                <span id="taskname">${taskInput.value}</span>
+                <button class="edit"><i class="fas fa-edit"></i></button>
+                <button class="delete"><i class="far fa-trash-alt"></i></button>
             </div>
         `;
 
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
+        // Delete Task
+        let deleteButtons = document.querySelectorAll(".delete");
+        deleteButtons.forEach((btn) => {
+            btn.onclick = function () {
                 this.parentNode.remove();
-            }
-        }
+            };
+        });
 
-        var tasks = document.querySelectorAll(".task");
-        for(var i=0; i<tasks.length; i++){
-            tasks[i].onclick = function(){
+        // Mark Task as Completed
+        let tasks = document.querySelectorAll(".task");
+        tasks.forEach((task) => {
+            task.onclick = function () {
                 this.classList.toggle('completed');
-            }
-        }
+            };
+        });
 
-        document.querySelector("#newtask input").value = "";
+        // Edit Task
+        let editButtons = document.querySelectorAll(".edit");
+        editButtons.forEach((editBtn) => {
+            editBtn.onclick = function () {
+                let taskText = this.parentNode.querySelector("#taskname");
+                let newText = prompt("Edit your task:", taskText.innerText);
+                if (newText !== null && newText.trim() !== "") {
+                    taskText.innerText = newText;
+                }
+            };
+        });
+
+        // Clear Input Field
+        taskInput.value = "";
     }
-}
+};
